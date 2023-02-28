e45451c2ebc82680a9f753d5bc03e07c48c3a7bd
rupeshkumar0807 <suryawanshi.rupeshkumar8792@gmail.com>
2023-02-28 09:34:04 +0530

    Icici project

---
+++ src/com/qa/paybills/InstantPayBills.java
@@ -0,0 +1,9 @@
+package com.qa.paybills;
+
+public class InstantPayBills 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Instant Pay Bills");
+	}
+}
---
+++ src/com/qa/transfer/OwnAccountTransfer.java
@@ -0,0 +1,9 @@
+package com.qa.transfer;
+
+public class OwnAccountTransfer
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Own Account Transfer");
+	}
+}
